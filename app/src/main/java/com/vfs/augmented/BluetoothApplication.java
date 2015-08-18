91e88035f7318b4bd833252bf21f4eff9ed8ac0b
andreiamfg <andreia.mfg@gmail.com>
2015-08-17 19:48:39 -0700

    Stuff

---
+++ app/src/main/java/com/vfs/augmented/BluetoothApplication.java
@@ -0,0 +1,13 @@
+package com.vfs.augmented;
+
+import android.app.Application;
+
+import com.vfs.augmented.bluetooth.BluetoothController;
+
+/**
+ * Created by andreia on 17/08/15.
+ */
+public class BluetoothApplication extends Application
+{
+    public BluetoothController _bluetoothController;
+}
--- app/src/main/java/com/vfs/augmented/activities/ConnectActivity.java
+++ app/src/main/java/com/vfs/augmented/activities/ConnectActivity.java
@@ -1,40 +1,114 @@
 package com.vfs.augmented.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.vfs.augmented.BluetoothApplication;
 import com.vfs.augmented.R;
+import com.vfs.augmented.bluetooth.BluetoothController;
+import com.vfs.augmented.bluetooth.BluetoothControllerReceiver;
+import com.vfs.augmented.bluetooth.DeviceListActivity;
 
-public class ConnectActivity extends ActionBarActivity {
-
+public class ConnectActivity extends ActionBarActivity implements BluetoothControllerReceiver
+{
+    BluetoothController _btController;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connect_activity);
+
+        ((BluetoothApplication)this.getApplicationContext())._bluetoothController = new BluetoothController(this, this);
+        _btController = ((BluetoothApplication)this.getApplicationContext())._bluetoothController;
+        _btController.onActivityStart();
     }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        _btController.onActivityResume();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_connect, menu);
+        getMenuInflater().inflate(R.menu.bluetooth_chat, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.secure_connect_scan:
+            {
+                // Launch the DeviceListActivity to see devices and do scan
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent, BluetoothController.REQUEST_CONNECT_DEVICE_SECURE);
+                return true;
+            }
+            case R.id.insecure_connect_scan:
+            {
+                // Launch the DeviceListActivity to see devices and do scan
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent, BluetoothController.REQUEST_CONNECT_DEVICE_INSECURE);
+                return true;
+            }
+            case R.id.discoverable:
+            {
+                // Ensure this device is discoverable by others
+                _btController.ensureDiscoverable();
+                return true;
+            }
         }
+        return false;
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void receiveMsg(String msg)
+    {
+        // Do Stuff
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            case BluetoothController.REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    _btController.connectDevice(data, true);
+                }
+                break;
+            case BluetoothController.REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    _btController.connectDevice(data, false);
+                }
+                break;
+            case BluetoothController.REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    // Bluetooth is now enabled, so set up a chat session
+                    _btController.setupChat();
+                }
+                else
+                {
+                    // User did not enable Bluetooth or an error occurred
+                    Toast.makeText(this, "not_enabled_leaving",
+                            Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
