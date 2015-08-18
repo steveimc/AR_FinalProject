3b63ba367f4e08cc32f33af9c75177d156292b97
steveimc <steveimc08@gmail.com>
2015-08-17 19:47:07 -0700

    ...

--- app/src/main/java/com/vfs/augmented/activities/ConnectActivity.java
+++ app/src/main/java/com/vfs/augmented/activities/ConnectActivity.java
@@ -7,31 +7,36 @@ import android.view.MenuItem;
 
 import com.vfs.augmented.R;
 
-public class ConnectActivity extends ActionBarActivity {
+public class ConnectActivity extends ActionBarActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connect_activity);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_connect, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
